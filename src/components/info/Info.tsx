b8c7a31a00b4eeb1952be325bbcee1acc4475f5a
jbee.han <jbee.han@navercorp.com>
2018-03-31 17:54:09 +0900

    feat: Add Info component

---
+++ src/components/info/Info.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: calc(100% - 20px);
+  height: 36px;
+  margin: auto;
+  margin-top: 52px;
+`;
+
+const Logo = styled.span`
+  font-size: 14px;
+`;
+
+const Button = styled.a`
+  color: #0366D6;
+  float: right;
+  font-size: 14px;
+  :hover {
+    color: #0366D6;
+    text-decoration: none;
+    cursor: pointer;
+    font-weight: bold;
+  }
+`;
+
+const Info = () => (
+  <Container>
+    <Logo>Octodirect</Logo>
+    <Button>Add permission</Button>
+  </Container>
+);
+
+export default Info;
