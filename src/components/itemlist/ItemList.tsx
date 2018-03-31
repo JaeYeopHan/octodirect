ccccf7e1056b2c00a8892d6c7d54aa0e1b6f15e4
jbee.han <jbee.han@navercorp.com>
2018-03-31 17:07:49 +0900

    refactor: Extract component: ItemList

---
+++ src/components/itemlist/ItemList.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import Item from '../item/Item';
+import { ItemType } from '../../model/item.model';
+
+export const Items = styled.ul`
+  position: relative;
+  top: 40px;
+  margin: auto 4px;
+  padding: 12px;
+  width: calc(100% - 36px);
+  height: 100%;
+  font-size: 14px;
+  font-weight: bold;
+  color: #24292E;
+  background-color: #FAFBFC;
+  border: solid 1px #E2E4E8;
+  border-radius: 3px;
+  overflow-y: scroll;
+`;
+
+interface ItemListProps {
+  filtered: ItemType[];
+  index: number;
+}
+
+class ItemList extends React.Component<ItemListProps, {}> {
+  render() {
+    const { filtered, index } = this.props;
+    return (
+      <Items>
+        {filtered.map((item: ItemType, i) => (
+          <Item
+            key={i}
+            item={item}
+            index={i}
+            currentIndex={index}
+          />))}
+      </Items>
+    );
+  }
+}
+
+export default ItemList;
