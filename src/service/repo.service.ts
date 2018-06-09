40061dca383938872978fe2bf9626f7e35872c22
jbee.han <jbee.han@navercorp.com>
2018-06-09 17:55:02 +0900

    :tada: feat: Extract ajax logic to service

---
+++ src/service/repo.service.ts
@@ -0,0 +1,9 @@
+const BASE_URL = `https://api.github.com/users`;
+const NAME = `JaeYeopHan`;
+
+export async function fetchRepos() {
+  const response = await fetch(`${BASE_URL}/${NAME}/repos`);
+  const repos = await response.json();
+
+  return repos;
+}
