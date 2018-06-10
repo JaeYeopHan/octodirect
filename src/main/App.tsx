6d88cef66d52af88de8c61887f8a5e703cc7fd85
jbee.han <jbee.han@navercorp.com>
2018-06-10 15:14:00 +0900

    :shower: Remove useless code

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -2,35 +2,15 @@ import * as React from 'react';
 import Input from '../components/input/Input';
 import ItemList from '../components/itemlist/ItemList';
 import Info from '../components/info/Info';
-import { Content } from '../styled-components/Content';
 import { Container } from '../styled-components/Container';
 import { Dispatch } from 'redux';
-import { fetchReposRequest } from '../actions/repo.action';
 
-interface AppProps {
-  dispatch: Dispatch;
-}
-
-class App extends React.Component<AppProps, {}> {
-  constructor(props: AppProps) {
-    super(props);
-  }
-
-  async componentDidMount() {
-    this.props.dispatch(fetchReposRequest());
-  }
-
-  render() {
-    return (
-      <Container>
-        <Input />
-        <Content>
-          <ItemList />
-        </Content>
-        <Info />
-      </Container>
-    );
-  }
-}
+const App = ({ dispatch }: { dispatch: Dispatch }) => (
+  <Container>
+    <Input />
+    <ItemList />
+    <Info />
+  </Container>
+);
 
 export default App;
--- src/styled-components/Content.ts
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const Content = styled.div`
-  margin-bottom: 12px;
-  height: calc(100% - 48px);
-`;
