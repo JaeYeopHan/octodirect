82073ef228344b604c972a365120bda18044c88f
jbee.han <jbee.han@navercorp.com>
2018-06-10 14:46:30 +0900

    :tada: feat: Extract actionTypes.ts

---
+++ src/actions/actionTypes.ts
@@ -0,0 +1,6 @@
+export const FETCH_REPOS_REQUEST: string = 'FETCH_REPOS_REQUEST';
+export const FETCH_SUCCESS: string = 'FETCH_SUCCESS';
+export const FETCH_FAIL: string = 'FETCH_FAIL';
+
+export const INDEX_DOWN: string = 'INDEX_DOWN';
+export const INDEX_UP: string = 'INDEX_UP';
