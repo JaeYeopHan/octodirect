f99fffdce6158c993e447649b3238e91748429e9
jbee.han <jbee.han@navercorp.com>
2018-06-12 21:02:56 +0900

    :wrench: fix: Change method for call api to `put` api

--- src/saga/repo.saga.ts
+++ src/saga/repo.saga.ts
@@ -1,10 +1,10 @@
-import { put } from 'redux-saga/effects';
+import { put, call } from 'redux-saga/effects';
 import { fetchRepos } from '../service/repo.service';
 import { FETCH_SUCCESS, FETCH_FAIL } from '../actions/actionTypes';
 
 export function* fetch(action?: any): any {
   try {
-    const repos = yield fetchRepos();
+    const repos = yield call(fetchRepos);
 
     yield put({ type: FETCH_SUCCESS, repos });
   } catch (error) {
--- src/service/repo.service.ts
+++ src/service/repo.service.ts
@@ -1,11 +1,9 @@
-import { repos } from '../main/test';
-
-// const BASE_URL = `https://api.github.com/users`;
-// const NAME = `JaeYeopHan`;
+const BASE_URL = `https://api.github.com/users`;
+const NAME = `JaeYeopHan`;
 
 export async function fetchRepos() {
-  // const response = await fetch(`${BASE_URL}/${NAME}/repos`);
-  // const repos = await response.json();
+  const response = await fetch(`${BASE_URL}/${NAME}/repos`);
+  const repos = await response.json();
 
   return repos;
 }
