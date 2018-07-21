37d00c8c85c8fc3664e5d97e75fb005e36ddc5d0
jbee.han <jbee.han@navercorp.com>
2018-07-21 17:48:07 +0900

    :tada: feat: Add development environment variable

--- src/service/repo.service.ts
+++ src/service/repo.service.ts
@@ -1,7 +1,11 @@
-const BASE_URL = `https://api.github.com/users`;
-const NAME = `JaeYeopHan`;
+import { repos as repoMock } from '../main/test';
 
 export async function fetchRepos() {
+  if (process.env.NODE_ENV === 'development') {
+    return repoMock;
+  }
+  const BASE_URL = `https://api.github.com/users`;
+  const NAME = `JaeYeopHan`;
   const response = await fetch(`${BASE_URL}/${NAME}/repos`);
   const repos = await response.json();
 
