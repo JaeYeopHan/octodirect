9496d05a033753c02211676824f2f89cf174d107
jbee.han <jbee.han@navercorp.com>
2018-07-28 19:35:28 +0900

    :white_check_mark: test: Add App.tsx test

--- src/main/App.test.tsx
+++ src/main/App.test.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import App from './App';
 import { configure, shallow } from 'enzyme';
+
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
 describe('<App /> shallow rendering', () => {
-  it('Render AppContainer', () => {
+  it('Render App test', () => {
     const wrapper = shallow(<App />);
-    console.log(wrapper.html());
+    expect(wrapper.length).toBe(1);
   });
 });
