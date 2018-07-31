ffa36a1d6c326e812993618c5f660a76c84b26d6
jbee.han <jbee.han@navercorp.com>
2018-07-31 18:42:33 +0900

    :tada: feat: Add storage class for using localStorage

---
+++ src/storage/LocalStorage.ts
@@ -0,0 +1,16 @@
+const localStorageKey = '#__octodirect_extension_key__#';
+
+export class LocalStorage {
+  public static getData<T>(): T | void {
+    const rawData = window.localStorage.getItem(localStorageKey);
+    if (rawData) {
+      return;
+    } else {
+      return JSON.parse(rawData as string);
+    }
+  }
+
+  public static setData<T>(data: T): void {
+    window.localStorage.setItem(localStorageKey, JSON.stringify(data));
+  }
+}
