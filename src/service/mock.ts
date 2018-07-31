a68ff50b5b1e4947072b413b1353f8729b8f27cf
jbee.han <jbee.han@navercorp.com>
2018-07-31 19:09:06 +0900

    :wrench: fix: Change interface of mock data

--- src/service/mock.ts
+++ src/service/mock.ts
@@ -2,56 +2,56 @@ export const repos = [
   {
     id: 1,
     name: 'testa',
-    htmlUrl: 'https://github.com/',
+    url: 'https://github.com/',
   },
   {
     id: 2,
     name: 'testb',
-    htmlUrl: 'https://www.facebook.com/',
+    url: 'https://www.facebook.com/',
   },
   {
     id: 3,
     name: 'testc',
-    htmlUrl: 'https://twitter.com/',
+    url: 'https://twitter.com/',
   },
   {
     id: 4,
     name: 'testd',
-    htmlUrl: 'http://www.naver.com/',
+    url: 'http://www.naver.com/',
   },
   {
     id: 5,
     name: 'teste',
-    htmlUrl: 'https://s.search.naver.com/p/around/search.naver',
+    url: 'https://s.search.naver.com/p/around/search.naver',
   },
   {
     id: 6,
     name: 'testf',
-    htmlUrl: 'https://egghead.io/',
+    url: 'https://egghead.io/',
   },
   {
     id: 7,
     name: 'testg',
-    htmlUrl: 'https://github.com/',
+    url: 'https://github.com/',
   },
   {
     id: 8,
     name: 'testh',
-    htmlUrl: 'https://www.facebook.com/',
+    url: 'https://www.facebook.com/',
   },
   {
     id: 9,
     name: 'testi',
-    htmlUrl: 'https://twitter.com/',
+    url: 'https://twitter.com/',
   },
   {
     id: 10,
     name: 'testj',
-    htmlUrl: 'http://www.naver.com/',
+    url: 'http://www.naver.com/',
   },
   {
     id: 11,
     name: 'testk',
-    htmlUrl: 'https://egghead.io/',
+    url: 'https://egghead.io/',
   },
 ];
