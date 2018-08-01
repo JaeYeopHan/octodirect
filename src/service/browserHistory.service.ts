3691d41085774419237bb5e35938aab69611940d
jbee.han <jbee.han@navercorp.com>
2018-08-01 17:26:02 +0900

    :tada: feat: Add feature to fetch history

---
+++ src/service/browserHistory.service.ts
@@ -0,0 +1,42 @@
+import { bookmarks as repoMock } from './mock';
+import { RepositoryInfo } from './githubRepository.service';
+import { HistoryItem } from './browserHistory.service';
+
+export interface HistoryItem {
+  id: string;
+  url?: string;
+  title?: string;
+  lastVisitTime?: number;
+  typedCount?: number;
+  visitCount?: number;
+}
+
+const filteredUrl = 'https://github.com/';
+const maxResults = 200;
+
+export const getVisitedGitHubUrls = async (): Promise<RepositoryInfo[]> => {
+  if (process.env.NODE_ENV === 'development') {
+    return repoMock;
+  }
+  const items = await new Promise<HistoryItem[]>((resolve, _) => {
+    chrome.history.search(
+      { text: '', maxResults },
+      (historyItems: HistoryItem[]) => {
+        resolve(historyItems);
+      },
+    );
+  });
+
+  return items
+    .filter(
+      (item: HistoryItem) =>
+        item.title && item.url && item.url.includes(filteredUrl),
+    )
+    .map(
+      (item: HistoryItem): RepositoryInfo => ({
+        id: item.id,
+        name: item.title as string,
+        url: item.url as string,
+      }),
+    );
+};
