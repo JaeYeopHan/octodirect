36fb0e1f262be4df22a62bbc938a630107f19c8a
jbee.han <jbee.han@navercorp.com>
2018-08-01 17:25:27 +0900

    :tada: feat: Add mock data for feature to bookmark

--- src/service/mock.ts
+++ src/service/mock.ts
@@ -1,57 +1,75 @@
 export const repos = [
   {
-    id: 1,
+    id: '1',
     name: 'testa',
     url: 'https://github.com/',
   },
   {
-    id: 2,
+    id: '2',
     name: 'testb',
     url: 'https://www.facebook.com/',
   },
   {
-    id: 3,
+    id: '3',
     name: 'testc',
     url: 'https://twitter.com/',
   },
   {
-    id: 4,
+    id: '4',
     name: 'testd',
     url: 'http://www.naver.com/',
   },
   {
-    id: 5,
+    id: '5',
     name: 'teste',
     url: 'https://s.search.naver.com/p/around/search.naver',
   },
   {
-    id: 6,
+    id: '6',
     name: 'testf',
     url: 'https://egghead.io/',
   },
   {
-    id: 7,
+    id: '7',
     name: 'testg',
     url: 'https://github.com/',
   },
   {
-    id: 8,
+    id: '8',
     name: 'testh',
     url: 'https://www.facebook.com/',
   },
   {
-    id: 9,
+    id: '9',
     name: 'testi',
     url: 'https://twitter.com/',
   },
   {
-    id: 10,
+    id: '10',
     name: 'testj',
     url: 'http://www.naver.com/',
   },
   {
-    id: 11,
+    id: '11',
     name: 'testk',
+    url: 'https://egghead.io/',
+  },
+];
+
+export const bookmarks = [
+  {
+    id: '12',
+    name: 'bookamrka',
+    url: 'https://twitter.com/',
+  },
+  {
+    id: '13',
+    name: 'bookamrkb',
+    url: 'http://www.naver.com/',
+  },
+  {
+    id: '14',
+    name: 'bookamrkc',
     url: 'https://egghead.io/',
   },
 ];
