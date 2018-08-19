7de071c9298f7007b824e98255d0edd6f7b3c13c
jbee.han <jbee.han@navercorp.com>
2018-08-19 16:48:27 +0900

    :gift: feat: Add loading component

--- src/components/main-view/itemlist/loading/Loading.tsx
+++ src/components/main-view/itemlist/loading/Loading.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import { ItemsLayout } from '../../../../styled-components/ItemsLayout';
 
 const LoadingElement = ItemsLayout.extend`
@@ -7,8 +9,27 @@ const LoadingElement = ItemsLayout.extend`
   text-align: center;
 `;
 
+const DonutLoading = styled.div`
+  @keyframes donut-spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+  display: inline-block;
+  border: 4px solid rgba(0, 0, 0, 0.1);
+  border-left-color: #016cd1;
+  border-radius: 50%;
+  width: 24px;
+  height: 24px;
+  margin-top: 64px;
+  animation: donut-spin 1.2s linear infinite;
+`;
+
 export const Loading: React.SFC = () => (
   <LoadingElement>
-    <div> Loading... </div>
+    <DonutLoading />
   </LoadingElement>
 );
