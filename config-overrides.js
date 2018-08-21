ff600257c25f83aaf477235bb954e37e7653991a
jbee.han <jbee.han@navercorp.com>
2018-08-21 14:37:40 +0900

    :recycle: env: Add rewireStringReplace function

--- config-overrides.js
+++ config-overrides.js
@@ -1,31 +1,58 @@
+'use strict';
+
 const webpack = require('webpack');
 const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
   .BundleAnalyzerPlugin;
+const manifestJson = require('./public/manifest.json');
+const StringReplacePlugin = require('string-replace-webpack-plugin');
 
 module.exports = (config, env) => {
-  config = rewireUglifyJS(config, env);
-  config = rewireBundleAnalyzer(config, env);
+  if (env === 'production') {
+    config = rewireUglifyJS(config, env);
+    config = rewireBundleAnalyzer(config, env);
+    config = rewireStringReplace(config, env);
+  }
   return config;
 };
 
 function rewireUglifyJS(config, env) {
-  env === 'production' &&
-    config.plugins.unshift(
-      new UglifyJsPlugin({
-        sourceMap: true,
-        compress: {
-          drop_console: true,
-          unused: true,
-          dead_code: true,
-        },
-      }),
-    );
+  config.plugins.unshift(
+    new UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        drop_console: true,
+        unused: true,
+        dead_code: true,
+      },
+    }),
+  );
 
   return config;
 }
 
 function rewireBundleAnalyzer(config, env) {
-  env === 'production' && config.plugins.push(new BundleAnalyzerPlugin());
+  config.plugins.push(new BundleAnalyzerPlugin());
+  return config;
+}
+
+function rewireStringReplace(config, env) {
+  console.log(`[INFO] octodirect version : ${manifestJson.version}`);
+
+  config.module.rules.push({
+    test: /(\.tsx)$/,
+    loader: StringReplacePlugin.replace({
+      replacements: [
+        {
+          pattern: /#__VERSION__#/gi,
+          replacement: function(match, p1, offset, string) {
+            return manifestJson.version;
+          },
+        },
+      ],
+    }),
+  });
+  config.plugins.push(new StringReplacePlugin());
+
   return config;
 }
