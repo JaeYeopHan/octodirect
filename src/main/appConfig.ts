003df51671b8e2c10913cbb3e0c8972be8426606
jbee.han <jbee.han@navercorp.com>
2018-08-22 16:06:38 +0900

    :wrench: fix: Change SETTING_GUIDE_LINK url

--- src/main/appConfig.ts
+++ src/main/appConfig.ts
@@ -2,4 +2,4 @@ export const MAX_COUNT_OF_HISTORY: number = 300;
 export const DEFAULT_FILTERING_URL: string = 'https://github.com/';
 export const GOOGLE_SEARCH_URL: string =
   'https://www.google.co.kr/search?q=github: ';
-export const SETTING_GUIDE_LINK: string = `https://bit.ly/2wcaoec`;
+export const SETTING_GUIDE_LINK: string = `http://bit.ly/2MucP6G`;
