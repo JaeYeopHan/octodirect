cdffa664db6c9d14710d3b7cbbd75503474154ee
JaeYeopHan <ljyhanll@gmail.com>
2018-11-30 14:23:05 +0900

    :construction: env: Setup test environment

---
+++ tests/unit/Array.test.ts
@@ -0,0 +1,13 @@
+import { deleteItem } from '../../src/utils/Array';
+
+describe('utils/Array.ts', () => {
+  it('', () => {
+    // Given
+
+    // When
+
+    // Then
+    console.log(deleteItem);
+    expect(1).toBe(1);
+  });
+});
